0fe3cbd47dfcc90eb8877223a9296375254a40a9
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-14 16:20:53 +0800

    Add a guess limit for the guessing game.

--- main.c
+++ main.c
@@ -5,11 +5,13 @@ int main() {
     /* Variables */
     const int SECRET_NUMBER = 5;
     int userGuess;
+    int numberOfGuesses = 3;
 
     /* Conditional while loop */
-    while (userGuess != SECRET_NUMBER) {
+    while (userGuess != SECRET_NUMBER && numberOfGuesses != 0) {
         printf("Enter a number: ");
         scanf("%d", &userGuess);
+        numberOfGuesses--;
     }
     printf("You win!\n");
 
